5f04bd89e026f55a7ad61b4df3c75d48650c3f1f
balqistrp <balqistretrianto@gmail.com>
2024-09-18 11:23:51 +0700

    login page pt 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,63 +1,51 @@
 @extends('layouts.app') @section('content')
-<link href="{{ asset('css/cssBalqist.css') }}" rel="stylesheet">
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span> @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password"> @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span> @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old( 'remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button> @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a> @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="container vh-100 d-flex">
+    <div class="justify-content-center w-50 m-auto align-items-center shadow rounded px-5 py-4">
+        <a href="{{ url('/') }}" class="h2 text-decoration-none"><span>&#60;</span></a>
+        <p class="text-center text-uppercase h2 fw-bold">login</p>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="form-outline mb-4">
+                <label for="email" class="fontColor1">{{ __('Email') }}</label>
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> @error('email')
+                <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span> @enderror
             </div>
-        </div>
+
+
+
+
+            <label for="password" class="fontColor1">{{ __('Password') }}</label>
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password"> @error('password')
+            <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span> @enderror
+
+
+
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old( 'remember') ? 'checked' : '' }}>
+                <label class="form-check-label" for="remember">
+                    {{ __('Remember Me') }}
+                </label>
+            </div>
+
+            <div class="text-end">
+                @if (Route::has('password.request'))
+                <a class="btn btn-link text-decoration-none fontColor1" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a> @endif
+            </div>
+
+            <button type="submit" class="my-3 py-2 button1 w-100 rounded-pill">
+                {{ __('Login') }}
+            </button>
+
+            <p class="my-4 font-bold text-center fw-bold">Belum punya akun ? <span><a class="text-decoration-none fw-normal fontColor1" href="{{ route('register') }}">register now</a></span> </p>
+
+        </form>
     </div>
 </div>
+
 @endsection
