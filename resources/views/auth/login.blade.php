72d0b0f332c130dff64e51660fccd350f481048d
balqistrp <balqistretrianto@gmail.com>
2024-09-25 10:19:36 +0700

    belom selesai

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,51 +1,22 @@
 @extends('layouts.app') @section('content')
-<div class="container vh-100 d-flex">
-    <div class="justify-content-center w-50 m-auto align-items-center shadow rounded px-5 py-4">
-        <a href="{{ url('/') }}" class="h2 text-decoration-none"><span>&#60;</span></a>
-        <p class="text-center text-uppercase h2 fw-bold">login</p>
+<div class="login_container">
+
+    <div class="login_form_container">
+        <img src="{{ asset ('image/logo2_ bpkad.png') }}" alt="Logo" class="logo_img">
         <form method="POST" action="{{ route('login') }}">
             @csrf
-            <div class="form-outline mb-4">
-                <label for="email" class="fontColor1">{{ __('Email') }}</label>
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> @error('email')
-                <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span> @enderror
-            </div>
+            <h1>{{ __('Login') }}</h1>
 
+            <input id="email" type="email" name="email" placeholder="Email" required autofocus>
+            <input id="password" type="password" name="password" placeholder="Password" required>
 
-
-
-            <label for="password" class="fontColor1">{{ __('Password') }}</label>
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password"> @error('password')
-            <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span> @enderror
-
-
-
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old( 'remember') ? 'checked' : '' }}>
-                <label class="form-check-label" for="remember">
-                    {{ __('Remember Me') }}
-                </label>
-            </div>
-
-            <div class="text-end">
-                @if (Route::has('password.request'))
-                <a class="btn btn-link text-decoration-none fontColor1" href="{{ route('password.request') }}">
-                    {{ __('Forgot Your Password?') }}
-                </a> @endif
-            </div>
-
-            <button type="submit" class="my-3 py-2 button1 w-100 rounded-pill">
-                {{ __('Login') }}
-            </button>
-
-            <p class="my-4 font-bold text-center fw-bold">Belum punya akun ? <span><a class="text-decoration-none fw-normal fontColor1" href="{{ route('register') }}">register now</a></span> </p>
-
+            <a href="{{ route('password.request') }}">{{ __('Lupa Kata Sandi') }}</a>
+            <button type="submit">{{ __('Masuk') }}</button>
         </form>
     </div>
-</div>
 
+
+    <div class="login_image_container"></div>
+
+</div>
 @endsection
